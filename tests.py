0f11421c2daf15e4f0bc9da2e6b48b393fe38a32
Maher Bisan <maherbisan95@gmail.com>
2020-05-30 11:31:40 +0300

    ,,

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 import hw2
 import pandas as pd
-from termcolor import colored
+from termcolor import colored, cprint
 
-file_name = 'tests/in/test2.txt'
+file_name = 'tests/in/test6.txt'
 l1 = hw2.readParseData(file_name) 
 res = hw2.calcCompetitionsResults(l1)
 
@@ -15,5 +15,7 @@ print(colored('\n \t\t------new list----\n ','yellow' ),filter_res)
 
 print(colored('\n \t\t------finla result----\n ','green' ),res)
 
+cprint('\n \t\t------Olympic winner---- ','cyan',attrs=['blink'] )
+hw2.partB(file_name)
 
 
